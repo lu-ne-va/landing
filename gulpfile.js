3dca0ccabd336783f02593fbd696a8b5c0056a98
urla <urla48@yandex.ru>
2015-08-16 22:22:45 +0300

    html done

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('less', function () {
     return gulp.src('./less/*.less')
+        .pipe(sourcemaps.init())
         .pipe(less())
-        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(sourcemaps.write())
+        //.pipe(minifyCss({compatibility: 'ie8'}))
         .pipe(gulp.dest('./app/css'));
 });
 
 gulp.task('watch', function(){
     gulp.watch('./less/*.less', ['less']);
-
+    gulp.watch([
+        'app/*.html',
+        'app/css/*.css'
+    ]).on('change', reload);
 });
+
+gulp.task('server', function () {
+    browserSync({
+        notify: true,
+        port: 9700,
+        server: {
+            baseDir: 'app/'
+        }
+    })
+});
+
+gulp.task('default', ['less', 'server', 'watch']);
