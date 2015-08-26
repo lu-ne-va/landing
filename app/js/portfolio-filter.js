efdd9520d0d8685b43e20bcac20ba64b18d1b296
urla <urla48@yandex.ru>
2015-08-26 23:23:47 +0300

    add active class for tabs in portfolio

--- app/js/portfolio-filter.js
+++ app/js/portfolio-filter.js
@@ -3,21 +3,42 @@ var filter = (function () {
         $items = $('.portfolio__item');
 
     return {
+        /**
+         * Фильтрует объекты
+         */
         run: function () {
-            var id = $(this).attr('id').split('-');
-            var dataValue = id.pop();
-            var dataType = id.join('-');
+            var $this = $(this),
+                id = $this.attr('id').split('-'),
+                dataValue = id.pop(),
+                dataType = id.join('-');
 
             if (dataValue == 'all') {
                 $items.show(0);
 
+                filter.addActiveClass($this);
+
+
             } else {
-                var $showItems = $items.filter('[data-' + dataType + '="'+dataValue+'"]');
+                var $showItems = $items.filter('[data-' + dataType + '="' + dataValue + '"]');
+
                 $showItems.show(0);
                 $items.not($showItems).hide(0);
+
+                filter.addActiveClass($this);
             }
         },
 
+        /**
+         * Добавляет табу активный класс
+         * @param $tab
+         */
+        addActiveClass: function ($tab) {
+            $tab.addClass('active').siblings().removeClass('active');
+        },
+
+        /**
+         * Подписчик на события
+         */
         subscribe: function () {
             $tab.on('click', this.run)
         }
