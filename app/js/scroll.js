13d293e6af3bfd607e01740c05fead7d3218e528
urla <urla48@yandex.ru>
2015-08-29 15:53:48 +0300

    JS: remove scroll-1

--- app/js/scroll.js
+++ app/js/scroll.js
@@ -1,92 +1,119 @@
 var $window = $(window),
-    $header = $(".header__container"),
-    headerState = false,
-    $slides = $('.slide');
+    $header = $('.header__container'),
+    $slides = $('.slide'),
+    headerHeight = 77,
+    $menuLinks = $('.menu__link');
+
+var scroll = (function () {
+    var headerState = false,
+        $activeSlide = $slides.filter('#home');
 
 
-$window.on('scroll', function () {
+    return {
+        init: function () {
+            var scrollTop = $window.scrollTop(),
+                viewportHeight = $window.height(),
+                changeHeader = viewportHeight - headerHeight;
 
-    var scrollTop = $window.scrollTop(),
-        viewportHeight = $window.height(),
-        changeHeader = viewportHeight - 77;
+            scroll.setHeaderState(scrollTop, changeHeader);
+            scroll.showSlideContentOnScroll(scrollTop, viewportHeight);
+            scroll.changeActiveMenuItemOnScroll(scrollTop);
+        },
 
-    Header.setHeaderState(scrollTop, changeHeader);
-    Content.showSlideContentOnScroll(scrollTop, viewportHeight);
-});
+        /**
+         * Make header slim on scroll
+         * @param scrollTop
+         * @param changeHeader
+         */
+        setHeaderState: function (scrollTop, changeHeader) {
+            if (scrollTop >= changeHeader && !headerState) {
+                $header.addClass('header__container_slim');
+                headerState = true;
+
+            } else if (scrollTop < changeHeader && headerState) {
+                $header.removeClass('header__container_slim');
+                headerState = false;
+            }
+        },
+
+        /**
+         * Show content inside slide on scroll
+         * @param scrollTop
+         * @param viewportHeight
+         */
+        showSlideContentOnScroll: function (scrollTop, viewportHeight) {
+            $slides.each(function () {
+
+                if (scrollTop >= ($(this).offset().top - viewportHeight / 2) && !$(this).hasClass('show')) {
+                    $(this).addClass('show');
+                }
+            });
+
+        },
+
+        changeActiveMenuItemOnScroll: function (scrollTop) {
+            var $currentSlide;
+            $slides.each(function () {
+                var $slide = $(this);
+
+                if ( scrollTop >= ($slide.offset().top - headerHeight)) {
+                    $currentSlide = $slide;
+                } else {
+                    return false;
+                }
+            });
 
 
-var Header = {
+            if ($currentSlide && $currentSlide.get(0) !== $activeSlide.get(0)) {
+                var menuId = $currentSlide.attr('id'),
+                    $menuLink = $menuLinks.filter('[href = #' + menuId + ']');
 
-    /**
-     * Make header slim on scroll
-     * @param scrollTop
-     * @param changeHeader
-     */
-    setHeaderState: function (scrollTop, changeHeader) {
-        if (scrollTop >= changeHeader && !headerState) {
-            $header.addClass("header__container_slim");
-            headerState = true;
+                if ($menuLink.length) {
+                    $activeSlide = $currentSlide;
+                    navigation.changeActiveClass($menuLink);
+                }
+            }
+        },
 
-        } else if (scrollTop < changeHeader && headerState) {
-            $header.removeClass("header__container_slim");
-            headerState = false;
+
+        subscribe: function () {
+            $window.on('scroll', this.init);
         }
     }
-};
+})();
 
-var Content = {
 
-    /**
-     * Show content inside slide on scroll
-     * @param scrollTop
-     * @param viewportHeight
-     */
-    showSlideContentOnScroll: function (scrollTop, viewportHeight) {
-        $slides.each(function () {
-            if (scrollTop >= ($(this).offset().top - viewportHeight / 2) && !$(this).hasClass('show')) {
-                $(this).addClass('show');
-            }
-        });
-
-    }
-};
+scroll.subscribe();
 
 $(function () {
-    Content.showSlideContentOnScroll(0, 0);
+    scroll.showSlideContentOnScroll(0, 0);
 });
 
-
-//Navigation scrolling
 
 var navigation = (function () {
 
     return {
-        scroll: function () {
-            //debugger;
+        scroll: function (e) {
 
+            e.preventDefault();
 
-            var menuId = $(this).attr("id").split('_'),
-                slideId = menuId.pop(),
-                $slide = $('#slide_'+slideId);
+            var $menuLink = $(this),
+                menuId = $menuLink.attr('href'),
+                $slide = $slides.filter(menuId);
 
             $('body').animate({
                 scrollTop: $slide.offset().top
             }, 1000);
-            navigation.removeActiveClass.apply(this);
+            navigation.changeActiveClass($menuLink);
         },
-        addActiveClass: function () {
 
-            $(this).addClass('menu__link_active');
-
-        },
-        removeActiveClass: function () {
+        changeActiveClass: function ($menuLink) {
             $('.menu__link_active').removeClass('menu__link_active');
-            navigation.addActiveClass.apply(this);
+            $menuLink.addClass('menu__link_active');
 
         },
         subscribe: function () {
-            $(".menu__link").on("click", this.scroll);
-            //$window.on('scroll', navigation.removeActiveClass);
+            $menuLinks.on('click', this.scroll);
         }
     }
 })();
